5c85abfaa8b1b0ddb3e8931a9ce3452f87ef0ddb
Giovanni Ciatto <giovanni.ciatto@unibo.it>
2022-03-14 10:28:02 +0100

    chore: comments for Herbrand.kt

--- exercise-2/src/main/kotlin/it/unibo/ise/lab/kr/Herbrand.kt
+++ exercise-2/src/main/kotlin/it/unibo/ise/lab/kr/Herbrand.kt
@@ -4,47 +4,91 @@ import it.unibo.tuprolog.core.Atom
 import it.unibo.tuprolog.core.Struct
 import it.unibo.tuprolog.core.Term
 
-data class Functor(val name: String, val arity: Int = 0)
+/**
+ * A type for functors, i.e. function names of given arities.
+ * A `n`-ary functor named `f` can be used to construct [Struct]ures having `n` arguments.
+ * Functors of arity equal to 0 are for constants, a.k.a. [Atom]s.
+ *
+ * @param name a [String] representing the symbol
+ * @param arity a non-negative [Int] representing the arity of the symbol
+ */
+data class Functor(val name: String, val arity: Int = 0) {
+    init { require(arity >= 0) }
+}
 
-fun herbrand(vararg functors: Functor): Sequence<Term> {
+/**
+ * Given a set of [functors], this method lazily enumerates the partial Herbrand universe `Hᵢ`, up to index [max].
+ * The algorithm implemented by this method is as follows:
+ * 1. let `H₀` be the set of [Atom]s, attained from the 0-ary names in [functors]
+ * 2. let `H₁` be the set of [Term]s attained by adding the [expansion] of `H₀` to `H₀` itself
+ * 3. let `H₂` be the set of [Term]s attained by adding the [expansion] of `H₁` to `H₁` itself
+ * 4. let `Hᵢ` be the set of [Term]s attained by adding the [expansion] of `Hᵢ₋₁` to `Hᵢ₋₁` itself
+ * 5. and so on, up to `i == [max]
+ * @param functors is the set of [Functor]s spawning the Herbrand universe
+ * @param max limits the computation of the Herbrand universe to `Hₘₐₓ`. Negative values are interpreted as infinity
+ * @return a [Sequence] lazily enumerating all the terms in `Hₘₐₓ`
+ */
+fun herbrand(vararg functors: Functor, max: Int = -1): Sequence<Term> {
     val constants = functors.filter { it.arity == 0 }.map { Atom.of(it.name) }.toSet()
     val functorsWithArity = functors.filter { it.arity > 0 }.toSet()
     return sequence {
-        yieldAll(constants)
-        val universe: MutableSet<Term> = constants.toMutableSet()
-        var expansion: Set<Term>
-        do {
-            expansion = expand(universe, functorsWithArity).filter { it !in universe }.toSet()
+        val universe: MutableSet<Term> = mutableSetOf()
+        var expansion: Set<Term> = constants
+        var i = max
+        while (i >= 0 && expansion.isNotEmpty()) {
             universe.addAll(expansion)
             yieldAll(expansion)
-        } while (expansion.isNotEmpty())
-    }
-}
-
-internal fun expand(universe: Set<Term>, functors: Set<Functor>): Sequence<Term> = sequence {
-    for ((name, arity) in functors) {
-        for (arguments in power(universe, arity)) {
-            yield(Struct.of(name, arguments))
+            expansion = expansion(universe, functorsWithArity).toSet()
+            i--
         }
     }
 }
 
-internal fun <T> power(items: Iterable<T>, n: Int): Sequence<List<T>> {
-    return when {
-        n <= 0 -> emptySequence()
-        n == 1 -> items.asSequence().map { listOf(it) }
-        else -> sequence {
-            for (item in items) {
-                for (smallerPower in power(items , n - 1)) {
-                    yield(listOf(item) + smallerPower)
+/**
+ * Given some partial Herbrand [universe] `Hᵢ`, and a set of [functors],
+ * this method yields the sequence of [Term]s to be added to `Hᵢ` to compute `Hᵢ₊₁`, i.e.:
+ * ```
+ * Hᵢ₊₁ - Hᵢ
+ * ```
+ * @param universe the partial Herbrand universe `Hᵢ`, spawned by [functors]
+ * @param functors a set of [Functor]s, i.e. function names of given arities
+ * @return the [Sequence] of [Term]s in `Hᵢ₊₁ - Hᵢ`
+ */
+internal fun expansion(universe: Set<Term>, functors: Set<Functor>): Sequence<Term> {
+    return sequence {
+        for ((name, arity) in functors) {
+            for (arguments in cartesianPower(universe, arity)) {
+                val term = Struct.of(name, arguments)
+                if (term !in universe) {
+                    yield(term)
                 }
             }
         }
     }
 }
 
-fun main() {
-    herbrand(Functor("a"), Functor("b"), Functor("f", 2)).take(50).forEach {
-        println(it)
+/**
+ * Given a set of [items] of type [T], and a non-negative integer [n],
+ * this method computes the  [n]-ary [Cartesian power](https://en.wikipedia.org/wiki/Cartesian_product#n-ary_Cartesian_power ) of [items], i.e.:
+ * ```
+ * itemsⁿ = items × items × ... × items (n times)
+ * ```
+ * @param T any type
+ * @param items a set of objects of type [T], coveniently represented as an [Iterable]
+ * @param n a non-negative [Int] representing the order of the power
+ * @return a [Sequence] of [List]s of [T] where each list contains [n] items
+ */
+internal fun <T> cartesianPower(items: Iterable<T>, n: Int): Sequence<List<T>> {
+    return when {
+        n < 0 -> throw IllegalArgumentException("Unexpected exponent: $n (should be non-negative)")
+        n == 0 -> emptySequence()
+        n == 1 -> items.asSequence().map { listOf(it) }
+        else -> sequence {
+            for (item in items) {
+                for (smallerPower in cartesianPower(items , n - 1)) {
+                    yield(listOf(item) + smallerPower)
+                }
+            }
+        }
     }
 }
