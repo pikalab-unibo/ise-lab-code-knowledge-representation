9686b52aa4845de329c43d4025fa187e8d15c763
Giovanni Ciatto <giovanni.ciatto@unibo.it>
2022-03-14 15:48:54 +0100

    fix: comments and tests for exercise 4

--- exercise-4/src/main/kotlin/it/unibo/ise/lab/kr/Analyze.kt
+++ exercise-4/src/main/kotlin/it/unibo/ise/lab/kr/Analyze.kt
@@ -4,8 +4,24 @@ import it.unibo.tuprolog.core.Struct
 import it.unibo.tuprolog.core.Term
 import it.unibo.tuprolog.theory.Theory
 
+/**
+ * A type carrying 3 sorts of information concerning a given theory:
+ * @param predications the set of predicate symbols possibly used in all heads and bodies of any clause in the theory
+ * @param functors the set of function symbols possibly used within any predicates of any clause in the theory
+ * @param variableNames the set of variables' names possibly used within all terms exploited by of any clause in the theory
+ */
 data class Analysis(val functors: Set<Symbol>, val predications: Set<Symbol>, val variableNames: Set<Symbol>)
 
+/**
+ * Given a [Theory], this method should compute
+ * - the set of predicate symbols possibly used in all heads and bodies of any clause in the theory
+ * - the set of function symbols possibly used within any predicates of any clause in the theory
+ * - the set of variables' names possibly used within all terms exploited by of any clause in the theory
+ * and wrap them up into an instance of [Analysis]
+ * @param theory the [Theory] to be analysed
+ * @return an instance of [Analysis] summarising the set of functors, predicates, and variables' names
+ * used with the [theory]
+ */
 fun analyze(theory: Theory): Analysis {
     val predications = enumeratePredicates(theory)
         .map { Symbol(it.functor, it.arity) }
--- exercise-4/src/test/kotlin/it/unibo/ise/lab/kr/TestAnalyses.kt
+++ exercise-4/src/test/kotlin/it/unibo/ise/lab/kr/TestAnalyses.kt
@@ -5,7 +5,7 @@ import kotlin.test.assertEquals
 
 class TestAnalyses {
     @Test
-    fun testParents() {
+    fun testAll() {
         val theory = Theories.run { parenthood + genders + grandParenthood + motherhood + fatherhood }
         val analysisResult = analyze(theory)
         val expectedFunctors = listOf(
@@ -23,6 +23,79 @@ class TestAnalyses {
             "sarah",
             "terah",
         ).map { Symbol(it) }.toSet()
+        val expectedPredicates = setOf(
+            Symbol("parent", 2),
+            Symbol("male", 1),
+            Symbol("female", 1),
+            Symbol("grandparent", 2),
+            Symbol("mother", 2),
+            Symbol("father", 2),
+        )
+        val expectedVariables = listOf("X", "Y", "Z").map { Symbol(it) }.toSet()
+        val expectedResult = Analysis(expectedFunctors, expectedPredicates, expectedVariables)
+        assertEquals(expectedResult, analysisResult)
+    }
+
+    @Test
+    fun testParenthood() {
+        val theory = Theories.run { parenthood }
+        val analysisResult = analyze(theory)
+        val expectedFunctors = listOf(
+            "abraham",
+            "beutel",
+            "haran",
+            "isaac",
+            "jacob",
+            "joseph",
+            "labam",
+            "milcah",
+            "nahor",
+            "rachel",
+            "rebecca",
+            "sarah",
+            "terah",
+        ).map { Symbol(it) }.toSet()
+        val expectedPredicates = setOf(
+            Symbol("parent", 2),
+        )
+        val expectedVariables = emptySet<Symbol>()
+        val expectedResult = Analysis(expectedFunctors, expectedPredicates, expectedVariables)
+        assertEquals(expectedResult, analysisResult)
+    }
+
+    @Test
+    fun testGenders() {
+        val theory = Theories.run { genders }
+        val analysisResult = analyze(theory)
+        val expectedFunctors = listOf(
+            "abraham",
+            "beutel",
+            "haran",
+            "isaac",
+            "jacob",
+            "joseph",
+            "labam",
+            "milcah",
+            "nahor",
+            "rachel",
+            "rebecca",
+            "sarah",
+            "terah",
+        ).map { Symbol(it) }.toSet()
+        val expectedPredicates = setOf(
+            Symbol("male", 1),
+            Symbol("female", 1),
+        )
+        val expectedVariables = emptySet<Symbol>()
+        val expectedResult = Analysis(expectedFunctors, expectedPredicates, expectedVariables)
+        assertEquals(expectedResult, analysisResult)
+    }
+
+    @Test
+    fun testIntensionals() {
+        val theory = Theories.run { grandParenthood + motherhood + fatherhood }
+        val analysisResult = analyze(theory)
+        val expectedFunctors = emptySet<Symbol>()
         val expectedPredicates = setOf(
             Symbol("parent", 2),
             Symbol("male", 1),
